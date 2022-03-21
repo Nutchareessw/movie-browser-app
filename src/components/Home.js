58dfd3c7b67f9547fd761d8c60a3dda4c40e3558
nutcharee (Sam) <79275381+Nutchareessw@users.noreply.github.com>
2022-03-21 23:49:35 +0800

    Update Home.js
    update home detail

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import Hero from './Hero'
 const Home = () => {
     return (
         <>
-            <Hero text="Welcome to React"/>
+            <Hero text="React Movie Cards"/>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-8 offset-lg-2 my-5">
                         <p className="lead">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        A basic application that displays a list of movies as a list of cards. Each card provides movie detail such as title, description, and image.
                         </p>
                     </div>
                 </div>
